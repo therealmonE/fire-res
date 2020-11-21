4a2216b5f0ee5f51baf245e62846545a67a9cea0
therealmonE <36332304+therealmone@users.noreply.github.com>
2020-11-21 21:31:38 +0300

    Changed reports scope to singleton

--- excess-pressure/src/main/java/io/github/therealmone/fireres/excess/pressure/ExcessPressureModule.java
+++ excess-pressure/src/main/java/io/github/therealmone/fireres/excess/pressure/ExcessPressureModule.java
@@ -1,6 +1,7 @@
 package io.github.therealmone.fireres.excess.pressure;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import io.github.therealmone.fireres.excess.pressure.factory.ExcessPressureFactory;
 import io.github.therealmone.fireres.excess.pressure.report.ExcessPressureReport;
 import io.github.therealmone.fireres.excess.pressure.report.ExcessPressureReportProvider;
@@ -9,7 +10,10 @@ public class ExcessPressureModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(ExcessPressureReport.class).toProvider(ExcessPressureReportProvider.class);
+        bind(ExcessPressureReport.class)
+                .toProvider(ExcessPressureReportProvider.class)
+                .in(Singleton.class);
+
         bind(ExcessPressureFactory.class);
     }
 
--- heat-flow/src/main/java/io/github/therealmone/fireres/heatflow/HeatFlowModule.java
+++ heat-flow/src/main/java/io/github/therealmone/fireres/heatflow/HeatFlowModule.java
@@ -1,6 +1,7 @@
 package io.github.therealmone.fireres.heatflow;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import io.github.therealmone.fireres.heatflow.factory.HeatFlowFactory;
 import io.github.therealmone.fireres.heatflow.report.HeatFlowReport;
 import io.github.therealmone.fireres.heatflow.report.HeatFlowReportProvider;
@@ -9,7 +10,11 @@ public class HeatFlowModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(HeatFlowReport.class).toProvider(HeatFlowReportProvider.class);
+        bind(HeatFlowReport.class)
+                .toProvider(HeatFlowReportProvider.class)
+                .in(Singleton.class);
+
         bind(HeatFlowFactory.class);
     }
+
 }
