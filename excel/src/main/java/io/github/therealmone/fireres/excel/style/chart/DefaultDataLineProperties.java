2894d4ddc87de2c636f8232a815934b60ea32198
therealmonE <36332304+therealmone@users.noreply.github.com>
2020-12-08 12:59:13 +0300

    Removed poorly visible colors

--- excel/src/main/java/io/github/therealmone/fireres/excel/style/chart/DefaultDataLineProperties.java
+++ excel/src/main/java/io/github/therealmone/fireres/excel/style/chart/DefaultDataLineProperties.java
@@ -1,19 +1,16 @@
 package io.github.therealmone.fireres.excel.style.chart;
 
 import lombok.val;
-import org.apache.poi.xddf.usermodel.PresetColor;
 import org.apache.poi.xddf.usermodel.XDDFColorPreset;
 import org.apache.poi.xddf.usermodel.XDDFLineProperties;
 import org.apache.poi.xddf.usermodel.XDDFSolidFillProperties;
 
-import static io.github.therealmone.fireres.core.utils.RandomUtils.generateValueInInterval;
 
 public class DefaultDataLineProperties extends XDDFLineProperties {
 
     public DefaultDataLineProperties() {
         val fillProperties = new XDDFSolidFillProperties();
-        val availableColors = PresetColor.values();
-        val color = new XDDFColorPreset(availableColors[generateValueInInterval(0, availableColors.length - 1)]);
+        val color = new XDDFColorPreset(AvailableColors.get());
 
         fillProperties.setColor(color);
 
