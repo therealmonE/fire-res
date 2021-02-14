78b30779544823bdf25320ad0818a5f7d82d6ae7
Jaxor1337 <48794008+Jaxor1337@users.noreply.github.com>
2021-02-14 14:56:34 +0300

    Fixed comments

--- gui/src/main/java/io/github/therealmone/fireres/gui/controller/MainSceneController.java
+++ gui/src/main/java/io/github/therealmone/fireres/gui/controller/MainSceneController.java
@@ -5,12 +5,10 @@ import io.github.therealmone.fireres.core.config.GenerationProperties;
 import io.github.therealmone.fireres.core.config.ReportType;
 import io.github.therealmone.fireres.gui.annotation.ChildController;
 import javafx.fxml.FXML;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.io.InputStream;
 import java.util.Arrays;
 
 @EqualsAndHashCode(callSuper = true)
@@ -46,8 +44,5 @@ public class MainSceneController extends AbstractController {
         topMenuBarController.postConstruct();
         generalParamsController.postConstruct();
         samplesTabPaneController.postConstruct();
-
-        InputStream defaultFont = getClass().getResourceAsStream("/font/OpenSans-Bold.ttf");
-        Font.loadFont(defaultFont, 12);
     }
 }
