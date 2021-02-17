3b039c0745fdc46879326cf65952672a4a9f8cb6
therealmone <36332304+therealmone@users.noreply.github.com>
2021-02-17 18:04:57 +0300

    Added alert window logo

--- gui/src/main/java/io/github/therealmone/fireres/gui/service/impl/AlertServiceImpl.java
+++ gui/src/main/java/io/github/therealmone/fireres/gui/service/impl/AlertServiceImpl.java
@@ -1,13 +1,19 @@
 package io.github.therealmone.fireres.gui.service.impl;
 
+import com.google.inject.Inject;
+import io.github.therealmone.fireres.gui.model.Logos;
 import io.github.therealmone.fireres.gui.service.AlertService;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 import lombok.val;
 
 public class AlertServiceImpl implements AlertService {
 
     public static final ButtonType OK = new ButtonType("ОК");
+
+    @Inject
+    private Logos logos;
 
     @Override
     public void showError(String text) {
@@ -17,6 +23,7 @@ public class AlertServiceImpl implements AlertService {
         alert.setHeaderText(null);
         alert.getDialogPane().getStylesheets().add("/css/style.css");
         alert.getDialogPane().getStyleClass().add("alert");
+        ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(logos.getLogo512());
 
         alert.showAndWait();
     }
